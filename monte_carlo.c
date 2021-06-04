d570a1a765c6edeee62003722e70a49df74936b1
samikmohan <85338110+samikmohan@users.noreply.github.com>
2021-06-04 13:47:45 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,41 +3,33 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
 
-float frandom() {
-  long int q = random();
-  float ret = (float)q/(float)RAND_MAX;
-  return ret;
+float mc_pi(int n)
+{	
+	int pc=0,ps=n;
+	
+	for(int i=0; i<n; i++){
+	float x,y,len;
+	x=frandom();
+	y=frandom();
+	len =x*x+y*y;
+	if(len<=1){
+	pc++;
+	}
+	}
+	float pi = 4.0*pc/ps;
+	return pi;
 }
-
-int main(void) {
-  float pi0;
-  float pi1;
-  
-  pi0 = mc_pi(25000);
-  pi1 = mc_pi(25000);
-  printf("%f %f\n", pi0, pi1);
-  
-  if (pi0 == pi1) {
-      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
-      abort();
-    }
-
-  if (fabs(pi0 - pi1) > 0.05) {
-      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
-      abort();
-  }
-
-    
-  for (int i=2000; i<5000; i++) {
-    pi0 = mc_pi(i);
-    if (!(fabs(pi0 - M_PI) < 0.4)) {
-      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
-      abort();
-    }
-  }
+OR
+float mc_pi(int n) {
+float num=0.0,x,y;
+for(int i=1;i<=n;i++) {
+	x=frandom();
+	y=frandom();
+	if(sqrt((x*x)+(y*y))<=1) {
+	num++;
+	}
 }
-
-
+return 4.0*(num/(float)n);
+}
 
